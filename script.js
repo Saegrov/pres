951198bdb6d2f0f03d1fa33e68eed304c302af25
Simen Saegrov <saegrov@gmail.com>
2014-09-15 21:02:42 +0200

    Oppgave 6

--- script.js
+++ script.js
@@ -13,3 +13,14 @@ function increaseLike() {
   likeCount++;
   likeSpan.innerHTML = likeCount;
 }
+
+
+function addComment() {
+  var inputComment = document.getElementById("commentInput");
+  var commentList = document.getElementById("comments");
+  var newListElement = document.createElement('li');
+
+  newListElement.innerHTML = inputComment.value;
+  commentList.appendChild(newListElement);
+  inputComment.value = '';
+}
